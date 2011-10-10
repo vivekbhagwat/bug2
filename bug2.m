fbd38c378726e4b0d9d2e2f8eb0d13d4711d6c5b
Nathan Hwang <thenoviceoof@gmail.com>
2011-10-09 22:11:55 -0400

    Added angle fixing, added some message displays

--- bug2.m
+++ bug2.m
@@ -5,40 +5,52 @@ function [ ] = bug2( serPort )
 
 td = 0.1; % time delta to wait
 fs = 0.1; % forward speed
-error = 0.5;
+as = 0.05; % angle speed (0, 0.2 m/s)
+goalError = 0.5; % distance from goal to hit
+angleError = 0.1; % radians, before we start going towards goal
 
 goal = [10, 0]; % assume goal is 10 meters in front of robot
 pos = [0,0,0]; % [x,y,theta]
 
+% drawing initialization
 figure(1) % set the active figure handle to figure 1
 clf; % clear figure 1
 hold on; %Set figure 1 not to clear itself on each call to plot
 
 while(1)
     % turn until we are pointing towards the goal
+    AngleSensorRoomba(serPort);
+    while(abs(pos(3)) > angleError)
+        turnAngle(serPort, as, -pos(3)/2);
+        pos(3) = pos(3) + AngleSensorRoomba(serPort);
+    end
     
-    % move towards goal
-    d = DistanceSensorRoomba(serPort);
-    [br,bl, wr,wl,wc, bf] = BumpsWheelDropsSensorsRoomba(serPort);
+    DistanceSensorRoomba(serPort); % clear distance
+    [br,bl, ~,~,~, bf] = BumpsWheelDropsSensorsRoomba(serPort);
     hit = (bf==1 || br==1 || bl==1);
+    % move towards goal
     SetFwdVelRadiusRoomba(serPort, fs, inf);
-    while(hit == 0 && dist(pos, goal)>error)
+    while(hit == 0 && dist(pos, goal)>goalError)
+        % go for a little while
         pause(td);
-        [br,bl, wr,wl,wc, bf] = BumpsWheelDropsSensorsRoomba(serPort);
+        % poll the bumpers
+        [br,bl, ~,~,~, bf] = BumpsWheelDropsSensorsRoomba(serPort);
         hit = (bf==1 || br==1 || bl==1);
         d = DistanceSensorRoomba(serPort);
         pos(1) = pos(1) + d;
+        % draw current location
         plot(plot(1), plot(2), 'o');
     end
-    % comments
-    if(dist(pos, goal)< error)
+    SetFwdVelRadiusRoomba(serPort, 0, inf);
+    % check if close to goal
+    if(dist(pos, goal)< goalError)
         disp('Goal reached');
         break
     end
-    SetFwdVelRadiusRoomba(serPort, 0, inf);
     d = DistanceSensorRoomba(serPort);
     pos(1) = pos(1) + d;
-    if(dist(pos, goal)< error)
+    % check again: stops aren't atomic
+    if(dist(pos, goal)< goalError)
         disp('Goal reached');
         break
     end
@@ -46,13 +58,16 @@ while(1)
 
     % wall follow
     pos = wallfollower(pos, goal);
+    % assume we aren't blocked
+    % check if we can reach the goal, or whether we reached the goal
     if(pos == false)
         disp('Cannot reach goal');
         break
-    elseif(dist(pos, goal)<error)
+    elseif(dist(pos, goal)<goalError)
         disp('Goal reached');
         break
     end
+    % draw location
     plot(plot(1), plot(2), 'o');
 end
 
