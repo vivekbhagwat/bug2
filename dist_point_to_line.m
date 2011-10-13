e318babecf2a39b92cd680df58b01729f522b2b5
Vivek Bhagwat <bhagwat.vivek@gmail.com>
2011-10-13 00:28:39 -0400

    dist_to_point working now

--- dist_point_to_line.m
+++ dist_point_to_line.m
@@ -1,4 +1,7 @@
 function d = dist_point_to_line(P0, P1, P2)
 %P1, P2, P0 row vectors of form [x, y]
-d = abs(det([P2-P1;P0-P1]))/abs(dist(P2-P1, [0,0]));
+% d = abs(det([P2-P1;P0-P1]))/abs(dist(P2,P1));
 
+
+d = abs((P2(1)-P1(1))*(P1(2)-P0(2)) - (P1(1)-P0(1))*(P2(2)-P1(2)));
+d = d / sqrt((P2(1)-P1(1))^2 + (P2(2)-P1(2))^2);
