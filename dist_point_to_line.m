44cefdd2163105d08169f77374ed118a6f18d15d
Nathan Hwang <thenoviceoof@gmail.com>
2011-10-12 23:37:25 -0400

    Fixed bug in dist_point_to_line, simulation runs with obstacles

--- dist_point_to_line.m
+++ dist_point_to_line.m
@@ -1,3 +1,4 @@
-function(P0, P1, P2)
+function d = dist_point_to_line(P0, P1, P2)
 %P1, P2, P0 row vectors of form [x, y]
-return abs(det([P2-P1;P0-P1]))/abs(P2-P1)
+d = abs(det([P2-P1;P0-P1]))/abs(dist(P2-P1, [0,0]));
+
